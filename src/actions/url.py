6d7b9608b6c6ceea56377df9fe28dec566529d22
さめたすたす <sharkpp@users.noreply.github.com>
2024-08-18 00:23:50 +0900

    fix exception catch block

--- src/actions/url.py
+++ src/actions/url.py
@@ -92,170 +92,172 @@ def _url(ctx, params):
       logger.debug("_url", "headers", headers)
       logger.debug("_url", "encoding", encoding)
       logger.debug("_url", "data", data)
+
+    #logger.debug("_url",method,url,encoding)
+  #  if cookies is not None:
+  #    for i, cookie in enumerate(cookies):
+  #      logger.debug("<%s>"%(i),cookie)
+
+    reqopts = {
+      "timeout": (REQUEST_CONNECT_TIMEOUT, REQUEST_RECV_TIMEOUT),
+    }
+    headers["User-Agent"] = UA
+    if cookies is not None:
+      reqopts["cookies"] = cookies
+    if 0 < len(headers):
+      reqopts["headers"] = headers
+
+    # クッキーを処理するためにリダイレクトを処理する
+    reqopts["allow_redirects"] = False
+
+    req = requests.session()
+    req.keep_alive = False
+
+    if "GET" == method:
+      try:
+        response = req.get(
+          url, **reqopts
+        )
+      except Timeout:
+        logger.warning("url", url, "timeout")
+        return False
+    elif "POST" == method:
+      # POST内容をコンテント種別ごとに処理を変えて準備
+      if data is not None:
+        content_Type = headers["Content-Type"].split(";")[0]
+        if "application/x-www-form-urlencoded" == content_Type:
+          data = urllib.parse.urlencode(data)
+        elif "application/json" == content_Type:
+          data = json.dumps(data)
+        reqopts["data"] = data
+      # POST
+      try:
+        response = req.post(
+          url, **reqopts
+        )
+      except Timeout:
+        logger.error("url", url, "timeout")
+        return False
+    else:
+      return False
+
+    if "allow_redirects" in reqopts and \
+        False == reqopts["allow_redirects"]:
+      # リダイレクトを処理する
+      history = []
+      if "data" in reqopts:
+        del reqopts["data"]
+      _url = url
+      while 300 == (math.floor(response.status_code / 100) * 100):
+        history.append(response)
+        history[len(history)-1].request.headers["cookie"] = cookies.get_dict() if cookies is not None else dict()
+        if re.search(r"^http:\/\/.+", response.headers["Location"][0]) is None: # 相対URL
+          _url = urljoin(_url, response.headers["Location"])
+        else: # 絶対URL
+          _url = response.headers["Location"]
+        logger.debug(">>>",_url)
+        logger.debug(">>>",type(response.cookies))
+        logger.debug(">>>",response.cookies.keys())
+        logger.debug(">>>",reqopts)
+        logger.debug(">>>",reqopts["cookies"].keys() if hasattr(reqopts, "cookies") else None)
+        # クッキーを更新する
+        if "cookies" not in reqopts:
+          cookies = reqopts["cookies"] = response.cookies
+        else:
+          reqopts["cookies"].update(response.cookies)
+        try:
+          response = req.get(
+            _url, **reqopts
+          )
+        except Timeout:
+          logger.warning(">>>url", url, "timeout")
+          return False
+        except Exception as err:
+          logger.warning(">>>url", url, err)
+          return False
+        logger.debug(">>>------------")
+      response.history = history
+      response.request.headers["cookie"] = cookies.get_dict() if cookies is not None else dict()
+
+    if encoding is None:
+      if HEADER_CONTENT_TYPE in response.headers:
+        m = re.search(r"^(text\/[a-zA-Z0-9.-]+|application\/json)(; *charset\s*=\s*([^;\s]+|.+$))?", response.headers[HEADER_CONTENT_TYPE])
+        if m is not None:
+          encoding = m.group(3) if m.group(3) is not None else "utf-8"
+        else:
+          encoding = "binary"
+
+    logger.debug(response.status_code)    # HTTPのステータスコード取得
+    logger.debug(response.text[0:64])    # レスポンスのHTMLを文字列で取得
+    logger.debug(response.headers)
+
+    # Last-Modified: <day-name>, <day> <month> <year> <hour>:<minute>:<second> GMT
+    # >> https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Last-Modified
+    content_last_modified_date = \
+      datetime.strptime(response.headers[HEADER_LAST_MODIFIED], "%a, %d %b %Y %H:%M:%S %Z") \
+        if HEADER_LAST_MODIFIED in response.headers \
+        else None
+  #  # Date: <day-name>, <day> <month> <year> <hour>:<minute>:<second> GMT
+  #  # >> https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Date
+  #  content_date = \
+  #    datetime.strptime(response.headers[HEADER_LAST_MODIFIED], "%a, %d %b %Y %H:%M:%S %Z") \
+  #      if HEADER_LAST_MODIFIED in response.headers \
+  #      else None
+
+    body = response.text
+    if encoding is not None:
+      if "binary" == encoding:
+        body = response.content
+      else:
+        body = response.content.decode(encoding)
+
+    logger.debug("==========================")
+    for i, hist in enumerate(response.history):
+      logger.debug('history<'+str(i)+'>.request.url:', hist.request.url)
+      logger.debug('history<'+str(i)+'>.request.body:', hist.request.body)
+      logger.debug('history<'+str(i)+'>.request.headers:', hist.request.headers)
+      logger.debug('history<'+str(i)+'>.status_code:', hist.status_code)
+      logger.debug('history<'+str(i)+'>.headers:', hist.headers)
+      logger.debug('history<'+str(i)+'>.cookies:', hist.cookies.get_dict())
+      logger.debug("--------------------------")
+    logger.debug('url:', url)
+    logger.debug('response.url:', response.url)
+    logger.debug('response.request.url:', response.request.url)
+    logger.debug('response.request.body:', response.request.body)
+    logger.debug('response.request.headers:', response.request.headers)
+    logger.debug('response.status_code:', response.status_code)
+    logger.debug('response.headers:', response.headers)
+    logger.debug('response.cookies:', response.cookies.get_dict())
+    logger.debug('body:', (body if type(body) is str else str(body))[0:1024])
+    logger.debug("==========================")
+
+    #if "Set-Cookie" in response.headers:
+    #  #logger.debug("Set-Cookie",response.headers["Set-Cookie"])
+    #  response.headers["Set-Cookie"] = (
+    #    re.sub(r"Domain=.+?(,\s*|$)", "",
+    #    re.sub(r"Expires=.+?;\s*", "", 
+    #      response.headers["Set-Cookie"]))
+    #  )
+    #  #logger.debug("Set-Cookie@",response.headers["Set-Cookie"])
+    #logger.debug("cookies",(response.cookies))
+
+    # クッキーをマージ
+    if cookies is not None:
+      response.cookies.update(reqopts["cookies"])
+
+    ctx.result_vars["res"] = {
+      "url": url,
+      "body": body,
+      "status": response.status_code,
+      "headers": dict(response.headers),
+      "timestamp": content_last_modified_date, # GMT
+      "cookies": response.cookies,
+      "response_url": response.url,
+    }
+
   except Exception as e:
     logger.error("_url", traceback.format_exc())
     return False
-
-  #logger.debug("_url",method,url,encoding)
-#  if cookies is not None:
-#    for i, cookie in enumerate(cookies):
-#      logger.debug("<%s>"%(i),cookie)
-
-  reqopts = {
-    "timeout": (REQUEST_CONNECT_TIMEOUT, REQUEST_RECV_TIMEOUT),
-  }
-  headers["User-Agent"] = UA
-  if cookies is not None:
-    reqopts["cookies"] = cookies
-  if 0 < len(headers):
-    reqopts["headers"] = headers
-
-  # クッキーを処理するためにリダイレクトを処理する
-  reqopts["allow_redirects"] = False
-
-  req = requests.session()
-  req.keep_alive = False
-
-  if "GET" == method:
-    try:
-      response = req.get(
-        url, **reqopts
-      )
-    except Timeout:
-      logger.warning("url", url, "timeout")
-      return False
-  elif "POST" == method:
-    # POST内容をコンテント種別ごとに処理を変えて準備
-    if data is not None:
-      content_Type = headers["Content-Type"].split(";")[0]
-      if "application/x-www-form-urlencoded" == content_Type:
-        data = urllib.parse.urlencode(data)
-      elif "application/json" == content_Type:
-        data = json.dumps(data)
-      reqopts["data"] = data
-    # POST
-    try:
-      response = req.post(
-        url, **reqopts
-      )
-    except Timeout:
-      logger.error("url", url, "timeout")
-      return False
-  else:
-    return False
-
-  if "allow_redirects" in reqopts and \
-      False == reqopts["allow_redirects"]:
-    # リダイレクトを処理する
-    history = []
-    if "data" in reqopts:
-      del reqopts["data"]
-    _url = url
-    while 300 == (math.floor(response.status_code / 100) * 100):
-      history.append(response)
-      history[len(history)-1].request.headers["cookie"] = cookies.get_dict() if cookies is not None else dict()
-      if re.search(r"^http:\/\/.+", response.headers["Location"][0]) is None: # 相対URL
-        _url = urljoin(_url, response.headers["Location"])
-      else: # 絶対URL
-        _url = response.headers["Location"]
-      logger.debug(">>>",_url)
-      logger.debug(">>>",response.cookies.keys())
-      logger.debug(">>>",reqopts)
-      logger.debug(">>>",reqopts["cookies"].keys() if hasattr(reqopts, "cookies") else None)
-      # クッキーを更新する
-      if "cookies" not in reqopts:
-        cookies = reqopts["cookies"] = response.cookies
-      else:
-        reqopts["cookies"].update(response.cookies)
-      try:
-        response = req.get(
-          _url, **reqopts
-        )
-      except Timeout:
-        logger.warning(">>>url", url, "timeout")
-        return False
-      except Exception as err:
-        logger.warning(">>>url", url, err)
-        return False
-      logger.debug(">>>------------")
-    response.history = history
-    response.request.headers["cookie"] = cookies.get_dict() if cookies is not None else dict()
-
-  if encoding is None:
-    if HEADER_CONTENT_TYPE in response.headers:
-      m = re.search(r"^(text\/[a-zA-Z0-9.-]+|application\/json)(; *charset\s*=\s*([^;\s]+|.+$))?", response.headers[HEADER_CONTENT_TYPE])
-      if m is not None:
-        encoding = m.group(3) if m.group(3) is not None else "utf-8"
-      else:
-        encoding = "binary"
-
-  logger.debug(response.status_code)    # HTTPのステータスコード取得
-  logger.debug(response.text[0:64])    # レスポンスのHTMLを文字列で取得
-  logger.debug(response.headers)
-
-  # Last-Modified: <day-name>, <day> <month> <year> <hour>:<minute>:<second> GMT
-  # >> https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Last-Modified
-  content_last_modified_date = \
-    datetime.strptime(response.headers[HEADER_LAST_MODIFIED], "%a, %d %b %Y %H:%M:%S %Z") \
-      if HEADER_LAST_MODIFIED in response.headers \
-      else None
-#  # Date: <day-name>, <day> <month> <year> <hour>:<minute>:<second> GMT
-#  # >> https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Date
-#  content_date = \
-#    datetime.strptime(response.headers[HEADER_LAST_MODIFIED], "%a, %d %b %Y %H:%M:%S %Z") \
-#      if HEADER_LAST_MODIFIED in response.headers \
-#      else None
-
-  body = response.text
-  if encoding is not None:
-    if "binary" == encoding:
-      body = response.content
-    else:
-      body = response.content.decode(encoding)
-
-  logger.debug("==========================")
-  for i, hist in enumerate(response.history):
-    logger.debug('history<'+str(i)+'>.request.url:', hist.request.url)
-    logger.debug('history<'+str(i)+'>.request.body:', hist.request.body)
-    logger.debug('history<'+str(i)+'>.request.headers:', hist.request.headers)
-    logger.debug('history<'+str(i)+'>.status_code:', hist.status_code)
-    logger.debug('history<'+str(i)+'>.headers:', hist.headers)
-    logger.debug('history<'+str(i)+'>.cookies:', hist.cookies.get_dict())
-    logger.debug("--------------------------")
-  logger.debug('url:', url)
-  logger.debug('response.url:', response.url)
-  logger.debug('response.request.url:', response.request.url)
-  logger.debug('response.request.body:', response.request.body)
-  logger.debug('response.request.headers:', response.request.headers)
-  logger.debug('response.status_code:', response.status_code)
-  logger.debug('response.headers:', response.headers)
-  logger.debug('response.cookies:', response.cookies.get_dict())
-  logger.debug('body:', (body if type(body) is str else str(body))[0:1024])
-  logger.debug("==========================")
-
-  #if "Set-Cookie" in response.headers:
-  #  #logger.debug("Set-Cookie",response.headers["Set-Cookie"])
-  #  response.headers["Set-Cookie"] = (
-  #    re.sub(r"Domain=.+?(,\s*|$)", "",
-  #    re.sub(r"Expires=.+?;\s*", "", 
-  #      response.headers["Set-Cookie"]))
-  #  )
-  #  #logger.debug("Set-Cookie@",response.headers["Set-Cookie"])
-  #logger.debug("cookies",(response.cookies))
-
-  # クッキーをマージ
-  if cookies is not None:
-    response.cookies.update(reqopts["cookies"])
-
-  ctx.result_vars["res"] = {
-    "url": url,
-    "body": body,
-    "status": response.status_code,
-    "headers": dict(response.headers),
-    "timestamp": content_last_modified_date, # GMT
-    "cookies": response.cookies,
-    "response_url": response.url,
-  }
 
   return True
 
